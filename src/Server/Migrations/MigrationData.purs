fc497827cde5b18701364f3d2b670d63865ddb61
John <john.a.cowie@gmail.com>
2020-06-11 10:57:43 +0100

    Migration for snapshots table

--- src/Server/Migrations/MigrationData.purs
+++ src/Server/Migrations/MigrationData.purs
@@ -19,10 +19,28 @@ createEventsTable id = {id, up, down, description}
         """
         description = "Create events table"
 
+createSnapshotsTable :: Int -> Migration Int String
+createSnapshotsTable id = {id, up, down, description}
+  where up = """
+          CREATE TABLE IF NOT EXISTS snapshots (
+            id INTEGER PRIMARY KEY,
+            app TEXT NOT NULL,
+            snapshot JSONB NOT NULL,
+            up_to_event INTEGER NOT NULL,
+            created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (up_to_event) REFERENCES events (id)
+          );
+        """
+        down = """
+          DROP TABLE IF EXISTS snapshots;
+        """
+        description = "Create snapshots table"
+
 
 migrationData :: Array (Migration Int String)
 migrationData = [
   createEventsTable 1
+, createSnapshotsTable 2
 ]
 
 validateMigrations :: Array (Migration Int String) -> Either String (Array (Migration Int String))
